c8e7d9d9595760d3a9e430b1fa032af6a76fddbb
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-05-19 14:10:24 -0400

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert').strict;
 const dboper = require('./operations'); //dboper is short for database operations - gives us access to operations.js
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-//to access server to mongoClient - 1st argument is url; 2nd is object that sets various options (useUnifiedTopology is good to use to help with deprecation); 3rd is a callback function
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  //first check to see that error is not null - 1st argument is value checking, 2nd is the expected value checking against to see if 1st argument strictly equals the 2nd
-  //if assert fails, it will throw an error and terminate the entire application & will console.log error
-  assert.strictEqual(err, null); //like shorthand for if(error===null) and so on
+//promise then method takes a callback function as its parameter
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
 
   console.log('Connected correctly to server');
 
-  const db = client.db(dbname); //method connects to the db database
+  const db = client.db(dbname);
 
   //delete everything that is already in campsites collection - allows test app to start with fresh blank document so there aren't issues in the future with testing (developers call it drop, not delete)
-  db.dropCollection('campsites', (err, result) => {
-    assert.strictEqual(err, null);
-    console.log('Dropped Collection', result); //true if it was successful
-
-    //insert document into that collection
-    dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites', result => { //result is inline function definition - we haven't called it yet
-      console.log('Insert Document:', result.ops); 
+  db.dropCollection('campsites')
+  .then(result => {
+    console.log('Dropped Collection', result); 
+  })
+  .catch(err => console.log("No collection to drop.")); //if no campsites to drop, we will log error but not close the connection
+  
+  dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test" }, 'campsites')
+  .then(result => { 
+    console.log('Insert Document:', result.ops); 
       
-      dboper.findDocuments(db, 'campsites', docs => {
-        console.log('Found Documents:', docs); //will log everything in campsites collection
+    return dboper.findDocuments(db, 'campsites');
+  })
+  .then(docs => {
+    console.log('Found Documents:', docs); 
 
-        dboper.updateDocument(db, {name: "Breadcrumb Trail Campground" }, //look for the document with name and update the description field of that document - will only update single doc (if more than one code will break)
-          {description: "Updated Test Description"}, 'campsites', result => {
-            console.log("Updated Document Count:", result.result.nModified); 
+    return dboper.updateDocument(db, {name: "Breadcrumb Trail Campground" }, {description: "Updated Test Description"}, 'campsites');
+  })
+  .then(result => {
+    console.log("Updated Document Count:", result.result.nModified); 
 
-            dboper.findDocuments(db, 'campsites', docs => { //get list of all docs in campsites collection
-              console.log("Found Documents:", docs);
+    return dboper.findDocuments(db, 'campsites');
+  })
+  .then(docs => { 
+    console.log("Found Documents:", docs);
 
-              dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites', result => {
-                console.log('Delted Document Count:', result.deletedCount);
+    return dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" }, 'campsites');
+   })
+  .then(result => {
+    console.log('Delted Document Count:', result.deletedCount);
 
-                client.close();
-              });
-            });
-          }
-        );
-      });
-    });
-  });
-});
+    return client.close();
+  })
+  .catch(err => {
+    console.log(err);
+    client.close();
+  });      
+})
+.catch(err => console.log(err));
--- operations.js
+++ operations.js
@@ -1,36 +1,22 @@
-const assert = require('assert').strict;
-
 //db we are using, document we want to insert, collection document is in, and callback function that will be called at the end of each method
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
   const coll = db.collection(collection); //gives application a way to access collection in a given database
-  coll.insertOne(document, (err, result) => {
-    assert.strictEqual(err, null);
-    callback(result); //callback function named callback will be defined elsewhere from another place in the code where a call to this insert document is made. Delivering result to the callback so callback can do what it needs to do.
-  });
+  return coll.insertOne(document); //this will now return a promise as its return value
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection); 
-  coll.find().toArray((err, docs) => {
-    assert.strictEqual(err, null);
-    callback(docs);
-  });
+  return coll.find().toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
   const coll = db.collection(collection);
-  coll.deleteOne(document, (err, result) => { //document is JS object that is passed in to figure out which document to delete
-    assert.strictEqual(err, null);
-    callback(result); //will get an object about info that was deleted
-  });
+  return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
   const coll = db.collection(collection);
-  coll.updateOne(document, { $set: update }, null, (err, result) => { //$set operator will write over existing info, 3rd parameter is for us to pass in certain optional configureations
-    assert.strictEqual(err, null);
-    callback(result);
-  });
+  return coll.updateOne(document, { $set: update }, null);
 };
 
 //these are similar to CRUD - insert/create, find/read, update/update, and remove/delete
